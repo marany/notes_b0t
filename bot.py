318a615086082816d60f174b4663d8c599afc636
big4oot <demidov.2013@yandex.ru>
2021-07-21 17:23:43 +0300

    Update bot.py
    Бот отвечает на "Привет"

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-#hello 
+import telebot
+from bot_config import *
+
+
+bot = telebot.TeleBot(bot_token)
+
+
+@bot.message_handler(content_types=['text'])
+def reaction_to_message(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, я бот, позволяющий создавать список заметок")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понял, напиши /start")
+
+
+bot.polling(none_stop=True, interval=0)
